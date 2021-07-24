5a1e0fc03cf7f542740bcd48515900d9e5a0e878
AlexStyx <shurarus96@gmail.com>
2021-07-24 11:51:20 +0300

    Build CoreDataservice

--- ProExpenser/Services /CoreDataService.swift
+++ ProExpenser/Services /CoreDataService.swift
@@ -7,7 +7,56 @@
 
 import Foundation
 import UIKit
+import CoreData
 
-class CoreDataServie {
-    let context = (UIApplication.shared as? AppDelegate)?.persistentContainer.viewContext
+final class CoreDataService {
+    private let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    
+    func addCategory(name: String, imageName: String) {
+        let entity = createEntity(entityKind: EntityKind.spendCategory)
+        let category = SpendCategory(entity: entity, insertInto: context)
+        category.name = name
+        category.imageName = imageName
+        saveContext()
+    }
+    
+    func addTransaction(for category: SpendCategory, date: Date, transitedValue: Double) {
+        guard let transactions = category.transactions?.mutableCopy() as? NSMutableOrderedSet else { return }
+        let entity = createEntity(entityKind: EntityKind.transaction)
+        let transaction = Transaction(entity: entity, insertInto: context)
+        transaction.date = date
+        transaction.transitedValue = transitedValue
+        transactions.add(transaction)
+        category.transactions = transactions
+        saveContext()
+    }
+    
+    func getCategories() -> [SpendCategory]? {
+        let fetchRequest: NSFetchRequest<SpendCategory> = SpendCategory.fetchRequest()
+        return try? context?.fetch(fetchRequest)
+    }
+    
+    func getTransactions(for category: SpendCategory) -> [Transaction]? {
+        return category.transactions?.mutableCopy() as? [Transaction]
+    }
+    
+    private func createEntity(entityKind: EntityKind) -> NSEntityDescription {
+        guard
+            let context = context,
+            let entity = NSEntityDescription.entity(forEntityName: entityKind.rawValue, in: context)
+        else {
+            fatalError("Cannot found context or entity")
+        }
+        return entity
+    }
+    
+    private func saveContext() {
+        do {
+            try context?.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
 }
---
+++ ProExpenser/SupportingFiles/EntityKind.swift
@@ -0,0 +1,13 @@
+//
+//  EntytyNames.swift
+//  ProExpenser
+//
+//  Created by Александр Бисеров on 7/24/21.
+//
+
+import Foundation
+
+enum EntityKind: String {
+    case spendCategory = "SpendCategory"
+    case transaction = "Transaction"
+}
