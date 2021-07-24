ff4f53187147a8eddb58a9fc27466050fa5a6d3c
AlexStyx <shurarus96@gmail.com>
2021-07-24 09:06:14 +0300

    Add main entities

---
+++ ProExpenser/Services /CoreDataService.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataService.swift
+//  ProExpenser
+//
+//  Created by Александр Бисеров on 7/24/21.
+//
+
+import Foundation
+import UIKit
+
+class CoreDataServie {
+    let context = (UIApplication.shared as? AppDelegate)?.persistentContainer.viewContext
+}
