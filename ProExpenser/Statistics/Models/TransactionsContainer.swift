79708b14d070ba9c55dcd49f9936994dbc38dbd6
AlexStyx <shurarus96@gmail.com>
2021-07-24 12:16:46 +0300

    Add TransactionContainer

---
+++ ProExpenser/Statistics/Models/TransactionsContainer.swift
@@ -0,0 +1,35 @@
+//
+//  TransactionsContainer.swift
+//  ProExpenser
+//
+//  Created by Александр Бисеров on 7/24/21.
+//
+
+import Foundation
+
+class TransactionContainer {
+    private var dayRange: ClosedRange<Date> = (Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date())...Date()
+    private var monthRange: ClosedRange<Date> = (Calendar.current.date(byAdding: .month, value: -1, to: Date()) ?? Date())...Date()
+    private var yearRange: ClosedRange<Date> = (Calendar.current.date(byAdding: .year, value: -1, to: Date()) ?? Date())...Date()
+    
+    var todayTransactions = [Transaction]()
+    var monthTransactions = [Transaction]()
+    var yearTransactions = [Transaction]()
+    
+    private func updateRanges() {
+        dayRange = (Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date())...Date()
+        monthRange = (Calendar.current.date(byAdding: .month, value: -1, to: Date()) ?? Date())...Date()
+        yearRange = (Calendar.current.date(byAdding: .year, value: -1, to: Date()) ?? Date())...Date()
+    }
+    
+    private func updateTransactionLists(transactions: [Transaction]) {
+        todayTransactions = transactions.filter { $0.date != nil ? dayRange.contains($0.date!) : false }
+        monthTransactions = transactions.filter { $0.date != nil ? monthRange.contains($0.date!) : false }
+        yearTransactions = transactions.filter { $0.date != nil ? yearRange.contains($0.date!) : false }
+    }
+    
+    init(transactions: [Transaction]) {
+        updateRanges()
+        updateTransactionLists(transactions: transactions)
+    }
+}
